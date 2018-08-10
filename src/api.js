caf3b5e4e56dd1f316be49a0be80150495f3a954
sirmilord <sir.milord@gmail.com>
2018-08-10 12:08:16 +0300

    Initial API include into project

---
+++ src/api.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+import NlpAccountsApi from "../bower_components/nlp-api/nlp-api-accounts.js";
+import NlpVaultApi from "../bower_components/nlp-api/nlp-api-vault.js";
+
+let apikey = "dc408573-6031-4d9f-93e1-b180085d06bd";
+
+window.api = {
+    set ticket(value) {
+        this.accounts.ticket = value;
+        this.vault.ticket = value;
+        if (value) localStorage.setItem('session-ticket', value); else localStorage.removeItem("session-ticket");
+    },
+    set onUnauthorized(handler) {
+        api.accounts.onUnauthorized = handler;
+        api.vault.onUnauthorized = handler;
+    },
+    accounts: new NlpAccountsApi("http://accounts.netlawplatform.com", apikey),
+    vault: new NlpVaultApi("http://vault.netlawplatform.com", apikey),
+};
+
+if (localStorage.getItem('session-ticket')) api.ticket = localStorage.getItem('session-ticket');
